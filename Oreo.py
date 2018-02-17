440204a6f2694fb5a28706e06694de8cbed082a1
Carlos Grillet <carlosgrillet5000@hotmail.com>
2018-02-17 04:54:08 -0400

    Update Oreo.py

--- Oreo.py
+++ Oreo.py
@@ -4,36 +4,40 @@ import telebot
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-bot = telebot.AsyncTeleBot("459912388:AAFhawlpahUapv4hq1pOANlevp6fepwrPS8")
+bot = telebot.AsyncTeleBot("<TOKEN>")
 
-Oreo = ChatBot('Oreo')
+Oreo = ChatBot('GrilletBot')
 Oreo.set_trainer(ListTrainer)
-	
+
+#Este for hace que el bot entrene las conversaciones en texto plano que estan dentro de la carpeta Data
+#que esta en la misma ruta que este archivo
 for archivo in os.listdir('Data'):
 	chats = open('Data/' + archivo, 'r').readlines()
 	Oreo.train(chats)
-
+#Respuesta al los comandos /help y /start
 @bot.message_handler(commands=["help", "start"])
 def enviar_saludo(message):
-	mensaje 	  = message.text
+	mensaje       = message.text
 	destinatario  = message.chat.id
 	bot.reply_to(message, "Estoy listo!")
-	bot.send_message(destinatario, "Hola, soy Oreo")
-
+	bot.send_message(destinatario, "Hola, soy GrilletBot")
+	
+#respuesta que en la que ChatterBot interactua
 @bot.message_handler(func=lambda message:True)
 def responder_mensaje(message):
-	mensaje 	  = message.text
+	mensaje       = message.text
 	destinatario  = message.chat.id
-	username 	  = message.chat.username
+	username      = message.chat.username
 	fechaMensaje  = message.date
-	respuesta 	  = str(Oreo.get_response(mensaje))
+	respuesta     = str(Oreo.get_response(mensaje))
 	if message.chat.type == "private":
 		bot.send_message(destinatario, respuesta)
 		#print(str(time.strftime("%D %H:%M", time.localtime(fechaMensaje))) + " " + username + ": " + mensaje)
 		#print("\t\t\t " + str(respuesta))
 	else:
 		bot.reply_to(message, respuesta)
-
+		
+#Respuesta a cualquier archivo multimedia que se le envie al bot
 @bot.message_handler(content_types=['document', 'photo' 'audio', 'video', 'voice', 'file'])
 def responder_multimedia(message):
 	bot.reply_to(message, "Que hago con esto?")
