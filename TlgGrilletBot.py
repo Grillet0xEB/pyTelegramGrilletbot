7f9b98980ec769e84f1ba2429e18f8764aa4bc54
Carlos Grillet <carlosgrillet5000@hotmail.com>
2018-02-18 03:05:04 -0400

    Update TlgGrilletBot.py

--- TlgGrilletBot.py
+++ TlgGrilletBot.py
@@ -53,7 +53,7 @@ def responder_mensaje(message):
 @bot.message_handler(content_types=['voice'])
 def responder_notaDeVoz(message):
 	destinatario  = message.chat.id
-	nota = open('Voz/nota.ogg', 'rb')
+	nota = open('Voz/voz.ogg', 'rb')
 	bot.send_chat_action(destinatario, 'record_audio')
 	bot.send_voice(destinatario, nota, 'Escuchame')
 
